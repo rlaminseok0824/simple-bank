6dbf5e43f2715275b4524cb60bcac10dfdf4bf76
rlaminseok0824 <114082574+rlaminseok0824@users.noreply.github.com>
2022-12-26 23:16:49 +0900

    Add viper(env/config)

---
+++ db/util/config.go
@@ -0,0 +1,26 @@
+package util
+
+import (
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	DBDriver      string `mapstructure:"DB_DRIVER"`
+	DBSource      string `mapstructure:"DB_SOURCE"`
+	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+}
+
+func LoadConfig(path string) (config Config, err error) {
+	viper.SetConfigName("app")
+	viper.AddConfigPath(path)
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
